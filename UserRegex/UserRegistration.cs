e06810d7a277016e4dca904d2cb403e60ddab76d
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-06 23:10:34 +0530

    [Pratibha Mastud] Add: Password Should have one Numeric number

--- UserRegex/UserRegistration.cs
+++ UserRegex/UserRegistration.cs
@@ -9,7 +9,7 @@ namespace UserRegex
     {
         public static bool regexPassword(string pass)
         {
-            string namePattern = "[A-Z]{1}";//Rule:2
+            string namePattern = "[0-9]{1}";//Rule:2
             Regex obj = new Regex(namePattern);
             return obj.IsMatch(pass);
         }
