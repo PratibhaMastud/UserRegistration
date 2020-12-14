e89b7368d573e674d1159b8adada2addb202b489
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-14 13:34:59 +0530

    Add : Write JUnit Test to Validate User Entry

--- UserRegex/Program.cs
+++ UserRegex/Program.cs
@@ -1,38 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace UserRegex
+namespace UserRegistrationProject
 {
     class Program
     {
-        static void Main(string[] args)
+         static void Main(string[] args)
         {
-			int index1 = 0;
-
-			// ASSIGING SOME VALID SAMPLES
-			string[] validEmailSample = { "abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com",
-				"abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com", "abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com",
-				".abc@abc.com", "abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com", "abc.@gmail.com",
-				"abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au" };
-
-			UserRegistration userRegistration = new UserRegistration();
-			while (index1 < validEmailSample.Length)
-			{
-				string email = validEmailSample[index1];
-				bool flag = userRegistration.regexEmail(email);
-				if (flag == true)
-				{
-					Console.WriteLine("This is valid  email");
-					Console.WriteLine("{0} :\t is the valid  email", email);
-				}
-				else
-				{
-					Console.WriteLine("This is Invalid  email");
-					Console.WriteLine("{0} : \t is the Invalid  email", email);
-
-				}
-				index1++;
-			}
-		}
-	}
+            Console.WriteLine("Welcome to User Registration ");
+            Console.WriteLine("Enter First Name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine(UserRegistration.validateFirstName(firstName));
+            Console.WriteLine("Enter Last Name");
+            string lastName = Console.ReadLine();
+            Console.WriteLine(UserRegistration.validateLastName(lastName));
+            Console.WriteLine("Enter Email Address");
+            string email = Console.ReadLine();
+            Console.WriteLine(UserRegistration.validateEmail(email));
+            Console.WriteLine("Enter Mobile Number");
+            string mobileNo = Console.ReadLine();
+            Console.WriteLine(UserRegistration.validateMobileNo(mobileNo));
+            Console.WriteLine("Enter password");
+            string password = Console.ReadLine();
+            Console.WriteLine(UserRegistration.validatePassword(password));
+        }
+    }
 }
 
